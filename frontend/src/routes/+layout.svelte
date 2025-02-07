9db3bc5c0439dfaf0446dc0106bcdec83c88054b
Moshe Rivkin <m0535541179@gmail.com>
2025-02-07 14:09:07 +0200

    chore: cleanup

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
 	import '@/app.css';
 	import { Toaster } from '@/lib/components/ui/sonner';
-	let { children } = $props();
+	const { children } = $props();
 </script>
 
-<main class="w-full h-screen overflow-hidden">
+<main class="h-screen w-full overflow-hidden">
 	{@render children()}
 </main>
 <Toaster richColors />
--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,5 +1,5 @@
-import adapter from "@sveltejs/adapter-static";
-import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
+import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -7,12 +7,10 @@ const config = {
 
 	kit: {
 		adapter: adapter(),
-
 		alias: {
-			"@/*": "./src/*",
-			"$lib/*": "./src/lib/*",
-		},
-	},
+			'@/*': './src/*'
+		}
+	}
 };
 
 export default config;
