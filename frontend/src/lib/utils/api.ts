1c899e17a8f07a64044df6fb393aae5d6ab1b002
malua <malua@users.noreply.github.com>
2025-04-20 16:18:18 +0200

    fix: deploy api under same domain

--- frontend/src/lib/utils/api.ts
+++ frontend/src/lib/utils/api.ts
@@ -2,5 +2,5 @@ import { hc } from 'hono/client';
 import type { AppType } from '@backend/index';
 
 export const { api } = hc<AppType>(
-	import.meta.env.PROD ? 'https://backend.malua.workers.dev' : 'http://localhost:8000'
+	import.meta.env.PROD ? 'https://api.malua.dev' : '/'
 );
